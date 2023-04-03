bc2b0bd208079a239337440cac9a562b3bb6099b
Guy Hennequin <guy.a.hennequin@gmail.com>
2023-04-03 22:35:54 +0900

    Update functions to set sketch pad size and change size.

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-// 5 mins 20 seconds
+//9min 53
 
-let pad = document.querySelector(".pad");
-pad.style.gridTemplateColumns = "repeat(16 , 1fr)"; //16 columns
-pad.style.gridTemplateRows = "repeat(16 , 1fr)"; //16 rows
+function padSize(number) { // function to set the size of the sketch pad
+    let pad = document.querySelector(".pad");
+    let grids = pad.querySelectorAll('div');
+    grids.forEach((div) => div.remove());
+    pad.style.gridTemplateColumns = `repeat(${number} , 1fr)`; //set number of columns
+    pad.style.gridTemplateRows = `repeat(${number} , 1fr)`; //set number of  rows
 
-for(let i = 0; i < 256; i++) { //16 x 16 - 256
-    let grid = document.createElement("div");
-    grid.style.backgroundColor = "blue";
-    pad.insertAdjacentElement("beforeend", grid);
+    let size = number * number; //columns x rows = sketch pad size
+    for(let i = 0; i < size; i++) { 
+        let grid = document.createElement("div");
+        //grid.addEventListener("mouseover", rainbowGrid);        
+        grid.addEventListener("mouseover", () => {
+            grid.style.backgroundColor = "black";
+        });
+        grid.style.backgroundColor = "blue";
+        pad.insertAdjacentElement("beforeend", grid);
+    }
+}
+
+padSize(16);
+
+function changePadSize(input) { // function to change the size of the sketch pad
+    if (input >= 1 || input <= 100) { // user can input between 1 and 100 only /////////NEED TO RESOLVE ISSUE/////////
+        padSize(input);
+    } else {
+        console.log("Please chose a size between 1 and 100.");
+    }
 }
