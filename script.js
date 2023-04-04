fac591a9653d7e5718413aa124b7bf03d4c8467d
Guy Hennequin <guy.a.hennequin@gmail.com>
2023-04-04 22:07:20 +0900

    Added colours and functions to manage all buttons

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-//9min 53
+let colour = "black"; 
+let click = true;
 
 function padSize(number) { // function to set the size of the sketch pad
     let pad = document.querySelector(".pad");
@@ -10,11 +11,11 @@ function padSize(number) { // function to set the size of the sketch pad
     let size = number * number; //columns x rows = sketch pad size
     for(let i = 0; i < size; i++) { 
         let grid = document.createElement("div");
-        //grid.addEventListener("mouseover", rainbowGrid);        
-        grid.addEventListener("mouseover", () => {
-            grid.style.backgroundColor = "black";
-        });
-        grid.style.backgroundColor = "blue";
+        grid.addEventListener("mouseover", rainbowGrid); // every new square will call the function      
+        //grid.addEventListener("mouseover", () => {
+        //    grid.style.backgroundColor = "black";
+        //});
+        grid.style.backgroundColor = "white";
         pad.insertAdjacentElement("beforeend", grid);
     }
 }
@@ -28,3 +29,27 @@ function changePadSize(input) { // function to change the size of the sketch pad
         console.log("Please chose a size between 1 and 100.");
     }
 }
+
+function rainbowGrid() { // function grid colours 
+    if (click) { // only colour sketch pad if click is true
+        if (colour === 'random') { // to manage random colours 
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`; // stack overflow equation
+        }  else {
+        this.style.backgroundColor = colour;
+        }
+    }
+}
+
+function changeColour(selection) { // function to change sketch colour of the squares called by the buttons in html
+    colour = selection;
+}
+
+function clearPad() {
+    let pad = document.querySelector(".pad");
+    let grids = pad.querySelectorAll('div');
+    grids.forEach((div) => div.style.backgroundColor = 'white');
+}
+
+document.querySelector("body").addEventListener("click", () => { // event listener to assign click to true or false
+    click = !click;
+});
